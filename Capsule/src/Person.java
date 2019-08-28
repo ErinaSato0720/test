cd866003fb2444f2b288be9ce551fd5293c52990
internousdev <sa10.19950720@gmail.com>
2019-08-28 16:34:12 +0900

    20190828

--- Capsule/src/Person.java
+++ Capsule/src/Person.java
@@ -1,21 +1,29 @@
 
-public class Person {
+public class Person{
 
-	public String name =null;
-	public int age = 0;
+	private String name = null;
+	private int age = 0;
 
-	public Person(String name, int age){
-		this.name=name;
-		this.age=age;
+	public Person(String name , int age){
+
+		this.name = name;
+		this.age= age;
 	}
 
 	public String getName(){
-
 		return this.name;
+	}
+
+	public int getAge(){
+		return this.age;
 	}
 
 	public void setName(String name){
 
-		this.name =name;
+		this.name = name;
+	}
+
+	public void setAge(int age){
+		this.age=age;
 	}
 }
